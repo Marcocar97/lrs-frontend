ccc36725e43373172a619ccec288a05b492df137
Marcos <mmarkito708@gmail.com>
2025-08-27 22:10:04 +0200

    new try 2.3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},   // evita errores cuando librerías esperan process.env
+  },
+  optimizeDeps: {
+    include: ['buffer'], // asegura que buffer esté disponible
+  },
 })
